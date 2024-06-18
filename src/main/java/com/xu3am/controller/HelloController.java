3accea065a946054d3f2d2fb7fe29f78a83bc47f
xu3woli <prostyliu@gmail.com>
2024-06-18 22:58:58 +0800

    init

---
+++ src/main/java/com/xu3am/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.xu3am.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("greeting", "Hello Amber");
+
+		return "helloworld";
+
+	}
+}
